9385d8bb426b2cfa07ccb8d8012a79caf680d0bf
Raphi__ <raphaellu.sem@gmail.com>
2026-02-06 15:14:10 -0500

    Import train_test_split from sklearn

--- Projects/facial_age_prediction/facial_age_helpers.py
+++ Projects/facial_age_prediction/facial_age_helpers.py
@@ -10,6 +10,7 @@ from tensorflow.keras import layers, models
 from tensorflow.keras.optimizers import Adam
 
 from tensorflow.keras.callbacks import ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
 
 import datetime
 
